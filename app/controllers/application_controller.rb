d797099c4f390bc6641f405f0c893e6d4d6ac0f4
Alessandro Varela <alessandrovarela@gmail.com>
2022-02-08 01:39:42 +0000

    add validation content-type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    unless request.get?      
+      return if request.headers["Content-Type"] =~ /vnd\.api\+json/
+      render :nothing => true, :status => :unsupported_media_type
+    end
+  end
+
 end
